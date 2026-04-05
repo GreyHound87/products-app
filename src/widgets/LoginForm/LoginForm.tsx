feed81fa0b7082f5fc99b19cc2bb97af42ad78f7
GreyHound87 <sasmirnov87@gmail.com>
2026-04-05 08:09:53 +0300

    refactor/self-review

--- src/widgets/LoginForm/LoginForm.tsx
+++ src/widgets/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Card, Flex, Form, theme } from 'antd'
+import { Card, Flex, Form } from 'antd'
 
 import styles from './LoginForm.module.scss'
 import LoginFormActions from './ui/LoginFormActions/LoginFormActions'
@@ -6,23 +6,11 @@ import LoginFormFields from './ui/LoginFormFields/LoginFormFields'
 import LoginFormHeader from './ui/LoginFormHeader/LoginFormHeader'
 
 const LoginForm = () => {
-  const { token } = theme.useToken()
   const [form] = Form.useForm()
 
   return (
     <Flex className={styles.wrapper}>
-      <Card
-        style={{
-          width: '100%',
-          borderRadius: 34,
-          border: '1px solid transparent',
-          background: `
-            linear-gradient(to bottom, ${token.colorBgLayout}, ${token.colorBgContainer} 50%) padding-box,
-            linear-gradient(to bottom, ${token.colorBorderSecondary}, ${token.colorBgContainer} 50%) border-box
-          `,
-        }}
-        classNames={{ body: styles.cardBody }}
-      >
+      <Card className={styles.card} classNames={{ body: styles.cardBody }}>
         <LoginFormHeader />
         <Form form={form} layout='vertical' requiredMark={false} size='large'>
           <LoginFormFields />
